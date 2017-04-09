4ebe1f2027a1c840fbbf4af73a8ba8a31638f21f
Rehan samarasekera <sewranga77@gmail.com>
2017-04-09 14:14:20 +0530

    Add files via upload

---
+++ main/java/com/example/rehans/streetsafe/Creat_account.java
@@ -0,0 +1,13 @@
+package com.example.rehans.streetsafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Creat_account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creat_account);
+    }
+}
---
+++ main/java/com/example/rehans/streetsafe/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.rehans.streetsafe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Login(View v){
+        Intent myIntentCreat = new Intent(this,Login.class);
+        startActivity(myIntentCreat);
+    }
+    public void CreatAccount(View v){
+        Intent myIntentCreat = new Intent(this,Creat_account.class);
+        startActivity(myIntentCreat);
+    }
+}
